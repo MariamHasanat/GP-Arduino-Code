cd00b92369f2dd05cbb0184fea844e909e04e8dd
MariamHasanat <201011@ppu.edu.ps>
2024-06-10 12:41:20 +0300

    code with new library

--- firebase-code.ino
+++ firebase-code.ino
@@ -2,24 +2,26 @@
 #include <WiFi.h>
 #include <math.h>
 #include <Arduino.h>
+#include <ArduinoJson.h>
+#include <HTTPClient.h>
 #include <FirebaseESP32.h>
-#include <Firebase_ESP_Client.h>
 #include <addons/TokenHelper.h>
 #include <addons/RTDBHelper.h>
-#include <ESP32Tone.h>
 
 #define SENSOR1_X_PIN 33
 #define SENSOR1_Y_PIN 34
 #define SENSOR1_Z_PIN 35
 #define NUM_SAMPLES 100
 #define NUM_OF_ITERATIONS 25
-#define WIFI_SSID "Ross****"
-#define WIFI_PASSWORD "dm3j9f@35*******"
+#define WIFI_SSID "Ross"
+#define WIFI_PASSWORD "dm3j9f@35"
 #define API_KEY "AIzaSyBgpUQqMd8SQj7ZsB33GVh8WJcD0jlD0pk"
 #define DATABASE_URL "https://app2-d200f-default-rtdb.firebaseio.com/"  //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 #define FIREBASE_PROJECT_ID "app2-d200f"                                //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
-#define USER_EMAIL "earhtquakeg***********raduationproject@gmail.com"
-#define USER_PASSWORD "earthqu*********ake123@456"
+#define USER_EMAIL "earhtquakegraduationproject@gmail.com"
+#define USER_PASSWORD "earthquake123@456"
+
+const int capacity = JSON_OBJECT_SIZE(3);
 
 struct DataPoint {
   double x;
@@ -40,15 +42,19 @@ int FinalResult = 0;
 int old_FinalResult = 0;
 int NUM_OF_ONES = 0;
 int num_of_iteration = 0;
+int predictedLabel;
 Parameters theta;
 DataPoint point;
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
+// FirebaseFirestore firestore;
 float AVG_ALERT = 0;
 float sensor1Data[3];                 // [X, Y, Z]
 float sensor1Buffer[3][NUM_SAMPLES];  // [X, Y, Z]
-
+float sensor1Features[3] = { 0 };     // Features for sensor 1 [stdX,  stdY, stdZ]
+DynamicJsonDocument doc(capacity);
+JsonObject docData = doc.to<JsonObject>();
 // Sigmoid function
 double sigmoid(double z) {
   return 1.0 / (1.0 + exp(-z));
@@ -86,9 +92,6 @@ void processSensorData() {
     sensor1Buffer[2][i] = analogRead(SENSOR1_Z_PIN);
     delay(0);  // Adjust delay based on sampling rate
   }
-  // Extract features
-  float sensor1Features[3] = { 0 };  // Features for sensor 1 [stdX,  stdY, stdZ]
-  int predictedLabel;
 
   for (int axis = 0; axis < 3; axis++) {
     sensor1Features[axis] = stddev(sensor1Buffer[axis], NUM_SAMPLES);  // here I fill the array of three values of STD
@@ -107,6 +110,7 @@ void processSensorData() {
 }
 
 void setup() {
+  Serial.begin(9600);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED) {
@@ -119,26 +123,31 @@ void setup() {
   Serial.println();
   Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
   config.api_key = API_KEY;
-  /* Assign the user sign in credentials */
   auth.user.email = USER_EMAIL;
   auth.user.password = USER_PASSWORD;
-  /* Assign the RTDB URL (required) */
-  config.database_url = DATABASE_URL;
-  /* Assign the callback function for the long running token generation task */
+  config.database_url = DATABASE_URL;                  /* Assign the callback function for the long running token generation task */
   config.token_status_callback = tokenStatusCallback;  // see addons/TokenHelper.h
-  Firebase.begin(&config, &auth);
-  // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
   Firebase.reconnectNetwork(true);
+  fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 /, 1024 / Tx buffer size in bytes from 512 - 16384 */, 1024);
+  Firebase.begin(&config, &auth);
+  Firebase.setDoubleDigits(5);
 }
 
 void loop() {
+
   num_of_iteration++;
   sensor1Data[0] = analogRead(SENSOR1_X_PIN);
   sensor1Data[1] = analogRead(SENSOR1_Y_PIN);
   sensor1Data[2] = analogRead(SENSOR1_Z_PIN);
   processSensorData();
+  HTTPClient http;
+  http.begin("https://app2-d200f.firebaseapp.com/api/v1/node_2/13");
+  http.addHeader("Content-Type", "application/json");
+  StaticJsonDocument<128> accelerationDocument;
+
   if (num_of_iteration == NUM_OF_ITERATIONS) {
     num_of_iteration = 0;
+
     AVG_ALERT = (NUM_OF_ONES / NUM_OF_ITERATIONS);
     if (AVG_ALERT > 0.3) {
       FinalResult = 1;
@@ -159,5 +168,13 @@ void loop() {
       NUM_OF_ONES = 0;
       Serial.println("Everythin is good ");
     }
+    // Prepare your document data
+    accelerationDocument["xAxis"] = sensor1Features[0];
+    accelerationDocument["yAxis"] = sensor1Features[1];
+    accelerationDocument["zAxis"] = sensor1Features[2];
+    char buffer[20];
+    serializeJson(accelerationDocument, buffer);
+    http.PATCH(buffer);
+    http.end();
   }
 }
